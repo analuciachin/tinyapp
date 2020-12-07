cda9262eabcbb1b32d0adabf41e6567582782280
analuciachin <ananime@gmail.com>
2020-12-07 21:06:47 +0000

    added a second route and template

--- express_server.js
+++ express_server.js
@@ -27,6 +27,12 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
